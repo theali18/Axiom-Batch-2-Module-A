11c1a1ccffefb0d0a9db5ea6a2dceee16bb3653d
theali18 <alihassanshah342@gmail.com>
2021-06-19 10:08:43 +0100

    Update script.js

--- project1/script.js
+++ project1/script.js
@@ -1,59 +1,70 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const cpassword = document.getElementById("cpassword");
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-// showError
-function showError(input, msg) {
-    const fControl = input.parentElement;
-    fControl.className = "form-control error";
-    const small = fControl.querySelector("small");
-    small.innerText = msg;
+//funtions
+//function to show error
+function showError(input,message){
+    const formControl = input.parentElement;
+    //.className override the class name
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+function showSucces(input){
+    const formControl = input.parentElement;
+    //.className override the class name
+    formControl.className = 'form-control success';
 }
 
-// showSuccess
-function showSuccess(input) {
-    const fControl = input.parentElement;
-    fControl.className = "form-control success";
+//function to check if the required fields have data
+function checkRequired(inputArray){
+    inputArray.forEach(function(input) {
+        if (input.value===''){
+            showError(input,`${getFieldId(input)} is Required!`);
+        }else{
+            showSucces(input);
+        }
+    });
 }
-
-// cEmail
-function cEmail(email) {
-    const re =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+//function to get the id of the field
+function getFieldId(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
 }
-
-// check data
-form.addEventListener("submit", (e) => {
+//function to check length
+function checkLength(input,min,max){
+    if(input.value.length <=min){
+        showError(input,`${getFieldId(input)} needs to be atleast ${min} characters `);
+    }else if(input.value.length > max){
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters `);
+    }else{
+        showSucces(input);
+    }
+}
+//function to check password and match with password2
+function checkPasswordsMatch(input1,input2){
+    if (input1.value !== input2.value){
+        showError(input2,"Password don't match!")
+    }
+}
+//function to check email
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(input.value.trim())){
+        showSucces(input);
+    }else{
+        showError(input,'Please Provide a valid email!')
+    }
+}
+//This is an event listerner for form on submit
+form.addEventListener('submit',function(e) {    
     e.preventDefault();
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,20);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
 
-    // username
-    if (username.value === "") {
-        showError(username, "Required");
-    } else if (username.value.length < 4) {
-        showError(username, "username must be greater the 3");
-    } else showSuccess(username);
-
-    // email
-    if (email.value === "") {
-        showError(email, "Required");
-    } else if (!cEmail(email.value)) {
-        showError(email, "Please Enter Valid Email");
-    } else showSuccess(email);
-
-    // password
-    if (password.value === "") {
-        showError(password, "Required");
-    } else if (password.value.length < 6) {
-        showError(password, "Password must be greater then 6");
-    } else showSuccess(password);
-
-    // cpassword
-    if (cpassword.value === "") {
-        showError(cpassword, "Required");
-    } else if (!(password.value === cpassword.value)) {
-        showError(cpassword, "Please Enter Same Password");
-    } else showSuccess(cpassword);
-});
+})
